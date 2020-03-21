6f8aa6c3822e4103f2559c97d0f1784dc1591b96
DosImpact <ypd03008@gmail.com>
2020-03-21 21:03:43 +0900

    1-2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 제로초 크롤링 강의
+
+# Sec01.
+
+npm i csv-parse
---
+++ index.js
@@ -0,0 +1,12 @@
+const parse = require("csv-parse/lib/sync");
+const fs = require("fs");
+
+const csv = fs.readFileSync("csv/data.csv"); //파일을 비동기적으로 읽는 메서드
+console.log(csv.toString("utf-8")); //버퍼를 문자열로 바꾸는데 인코딩을 정해준다.
+
+const records = parse(csv.toString("utf-8"));
+console.log(records);
+
+records.forEach((e, i) => {
+  console.log(`${i}|영화 제목 : ${e[0]} -> URL : ${e[1]} `);
+});

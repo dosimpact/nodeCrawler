f830956dd540f16a3925eee48f9e3103bfff07e0
DosImpact <ypd03008@gmail.com>
2020-03-29 01:32:20 +0900

    sec4 - splash crawling

--- Docs/Section04.md
+++ Docs/Section04.md
@@ -56,10 +56,73 @@ El.parentElement.removeChild(El);
 - 스크롤 내리기 명령 ( 너무 쉽다. )
 
 ```js
+DOM에서;
 window.scrollBy(0, 1000); //상대 좌표
 window.scrollTo(0, 0); //절대 좌표
 
 await page.waitForSelecctor("");
+```
+
+- 최종
+
+```js
+import pt from "puppeteer";
+import axios from "axios";
+import fs from "fs";
+
+const crwaler = async () => {
+  try {
+    const browser = await pt.launch({
+      headless: false,
+      args: ["--Window-size=1920.1080"]
+    });
+    const page = await browser.newPage();
+    page.setViewport({ width: 1920, height: 1080 });
+    await page.goto("https://unsplash.com/", { waitUntil: "networkidle2" });
+
+    //page 가 close 될때 reset 해주어 한다. 안그러면 leak
+    const infiniteScroll = async () => {
+      await page.evaluate(() => {
+        window.scrollBy(0, -100);
+        window.scrollBy(0, 1000);
+      });
+    };
+    const infiniteScrollEvent = setInterval(infiniteScroll, 100);
+
+    let imgSrcs = [];
+    while (imgSrcs.length <= 300) {
+      await page.waitForSelector(".nDTlD");
+      const res = await page.evaluate(() => {
+        const imgSrcs = [];
+        let containerEls = document.querySelectorAll(".nDTlD");
+        try {
+          containerEls.forEach(E => {
+            const imgEl = E.querySelector("img._2zEKz");
+            if (imgEl) {
+              const imgSrc = imgEl.getAttribute("src");
+              if (imgSrc) {
+                imgSrcs.push(imgSrc);
+                E.parentElement.removeChild(E);
+              }
+            }
+          });
+        } catch (error) {}
+        return imgSrcs;
+      });
+      imgSrcs = imgSrcs.concat(res);
+      console.log(" ✅ crwaling... ", imgSrcs.length);
+    }
+    console.log(imgSrcs);
+    console.log(" ✅ Successfull!! ", imgSrcs.length);
+    clearInterval(infiniteScrollEvent);
+    await page.close();
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+crwaler();
 ```
 
 # 4-5. 크롤링 결과를 파일로 만들기
--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 import pt from "puppeteer";
-const start = async () => {
-  const browser = await pt.launch({
-    headless: false,
-    args: ["--Window-size=1920,1080"]
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
+import axios from "axios";
+import fs from "fs";
 
-  await page.goto("https://movie.naver.com/movie/bi/mi/basic.nhn?code=72363");
-  await page.setRequestInterception(true);
-  page.on("request", request => {
-    if (request.resourceType() === "image") request.abort();
-    else request.continue();
-  });
-  await page.waitFor(5000);
-  await browser.close();
+const crwaler = async () => {
+  try {
+    const browser = await pt.launch({
+      headless: false,
+      args: ["--Window-size=1920.1080"]
+    });
+    const page = await browser.newPage();
+    page.setViewport({ width: 1920, height: 1080 });
+    await page.goto("https://unsplash.com/", { waitUntil: "networkidle2" });
+
+    //page 가 close 될때 reset 해주어 한다. 안그러면 leak
+    const infiniteScroll = async () => {
+      await page.evaluate(() => {
+        window.scrollBy(0, -100);
+        window.scrollBy(0, 1000);
+      });
+    };
+    const infiniteScrollEvent = setInterval(infiniteScroll, 100);
+
+    let imgSrcs = [];
+    while (imgSrcs.length <= 300) {
+      await page.waitForSelector(".nDTlD");
+      const res = await page.evaluate(() => {
+        const imgSrcs = [];
+        let containerEls = document.querySelectorAll(".nDTlD");
+        try {
+          containerEls.forEach(E => {
+            const imgEl = E.querySelector("img._2zEKz");
+            if (imgEl) {
+              const imgSrc = imgEl.getAttribute("src");
+              if (imgSrc) {
+                imgSrcs.push(imgSrc);
+                E.parentElement.removeChild(E);
+              }
+            }
+          });
+        } catch (error) {}
+        return imgSrcs;
+      });
+      imgSrcs = imgSrcs.concat(res);
+      console.log(" ✅ crwaling... ", imgSrcs.length);
+    }
+    console.log(imgSrcs);
+    console.log(" ✅ Successfull!! ", imgSrcs.length);
+    clearInterval(infiniteScrollEvent);
+    await page.close();
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-start();
+crwaler();
